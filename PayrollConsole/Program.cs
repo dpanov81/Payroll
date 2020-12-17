aab3a7a02a7abd1bb7d4193a8e85c7697d188f20
Denis <panovds72@gmail.com>
2020-12-17 20:46:33 +0500

    Add project files.

---
+++ PayrollConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
